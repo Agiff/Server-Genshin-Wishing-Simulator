dd9244bb8c49fa54aa77dde0b86def7ba0b073e7
Agiff <ghifarimohammadrahadian@gmail.com>
2023-02-20 01:44:38 +0700

    feat: add findInventory by id

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -11,6 +11,18 @@ class inventoryController {
       next(error);
     }
   }
+
+  static async findInventory(req, res, next) {
+    try {
+      const { id } = req.params;
+      const inventory = await Inventory.findByPk(id, {
+        include: [Character, Weapon]
+      })
+      res.status(200).json(inventory);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = inventoryController;
--- routes/inventoryRouter.js
+++ routes/inventoryRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const inventoryController = require('../controllers/inventoryController');
 
 router.get('/', inventoryController.showInventory);
+router.get('/:id', inventoryController.findInventory);
 
 module.exports = router;
