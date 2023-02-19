c473a453cdd570f0a6ad3181183bb02f40070bd6
Agiff <ghifarimohammadrahadian@gmail.com>
2023-02-19 23:17:23 +0700

    feat: login, add jwt & dotenv, and update errorHandler

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const { comparePassword } = require('../helpers/bcrypt');
+const { createToken } = require('../helpers/jwt');
 const { User } = require('../models');
 
 class userController {
@@ -10,6 +12,29 @@ class userController {
       const { username, email, password } = req.body;
       const createdUser = await User.create({ username, email, password });
       res.status(201).json(createdUser);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async login(req, res, next) {
+    try {
+      const { username, email, password } = req.body;
+      if (!username && !email || !password) throw { name: 'UsernameEmailPasswordRequired' };
+
+      let option;
+      if (username) option = { where: { username } };
+      else if (email) option = { where: { email } };
+      const userFound = await User.findOne(option);
+      if (!userFound) throw { name: 'UsernameEmailPasswordInvalid' };
+
+      const verifiedPassword = comparePassword(password, userFound.password);
+      if (!verifiedPassword) throw { name: 'UsernameEmailPasswordInvalid' };
+
+      const payload = { id: userFound.id };
+      const access_token = createToken(payload);
+
+      res.send({ access_token });
     } catch (error) {
       next(error);
     }
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -3,10 +3,19 @@ const errorHandler = (err, req, res, next) => {
   switch (err.name) {
     case 'SequelizeValidationError':
     case 'SequelizeUniqueConstraintError':
-      res.status(400).json({ message: err.errors[0].message })
+      res.status(400).json({ message: err.errors[0].message });
+      break;
+
+    case 'UsernameEmailPasswordRequired':
+      res.status(400).json({ message: 'Username/Email or Password is required' });
+      break;
+
+    case 'UsernameEmailPasswordInvalid':
+      res.status(401).json({ message: 'Username/Email or Password is invalid' });
       break;
   
     default:
+      res.status(500).json({ message: 'Internal Server Error' });
       break;
   }
 }
