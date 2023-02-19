e558141b394b0e36372b7ef87a3e022c2bf86ea8
Agiff <ghifarimohammadrahadian@gmail.com>
2023-02-20 01:34:33 +0700

    feat: add association on models, update showInventory

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,10 +1,12 @@
-const { Inventory } = require('../models');
+const { User, Inventory, Character, Weapon } = require('../models');
 
 class inventoryController {
   static async showInventory(req, res, next) {
     try {
-      const inventories = await Inventory.findAll();
-      res.send(inventories);
+      const inventories = await Inventory.findAll({
+        include: User
+      });
+      res.status(200).json(inventories);
     } catch (error) {
       next(error);
     }
