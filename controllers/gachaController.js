59a94211938ffc3c1a84058c611f8a80f8ff5bcd
Agiff <ghifarimohammadrahadian@gmail.com>
2023-02-27 22:30:18 +0700

    fix: miscount on pity and optimalize the counter

--- controllers/gachaController.js
+++ controllers/gachaController.js
@@ -12,11 +12,68 @@ class GachaController {
         include: [Pity]
       });
 
+      //Increase Pity Count
+      if (currentBanner.type === 'limitedChar') {
+        currentUser.Pity.charLimitedGoldPity++,
+        currentUser.Pity.charLimitedPurplePity++
+      } else if (currentBanner.type === 'limitedWeapon') {
+        currentUser.Pity.weaponLimitedGoldPity++,
+        currentUser.Pity.weaponLimitedPurplePity++
+      } else {
+        currentUser.Pity.standardGoldPity++,
+        currentUser.Pity.standardPurplePity++
+      }
+
       const rate = 100000;
       let goldRate = 600;
       const purpleRate = 5100;
 
       const result = Math.ceil(Math.random() * rate);
+
+      let gotGold = false;
+      let gotPurple = false;
+
+      if (currentUser.Pity.charLimitedPurplePity >= 10) { //Guara at 10 pull
+        gotPurple = true;
+      } else if (result <= purpleRate) { 
+        gotPurple = true;
+      }
+
+      if (currentUser.Pity.charLimitedGoldPity >= 76) { //Add chance to 20%
+        goldRate = 20000;
+      }
+
+      if (currentUser.Pity.charLimitedGoldPity >= 90) { //Guara 5 star at 90
+        gotPurple = false;
+        gotGold = true;
+      } else if (result <= goldRate) {
+        gotPurple = false;
+        gotGold = true;
+      }
+
+      //Update Pity
+      if (currentBanner.type === 'limitedChar') {
+        await Pity.update({
+          charLimitedGoldPity: gotGold ? 0 : currentUser.Pity.charLimitedGoldPity,
+          charLimitedPurplePity: gotPurple ? 0 : currentUser.Pity.charLimitedPurplePity
+        }, {
+          where: { UserId: req.user.id }
+        })
+      } else if (currentBanner.type === 'limitedWeapon') {
+        await Pity.update({
+          weaponLimitedGoldPity: gotGold ? 0 : currentUser.Pity.weaponLimitedGoldPity,
+          weaponLimitedPurplePity: gotPurple ? 0 : currentUser.Pity.weaponLimitedPurplePity
+        }, {
+          where: { UserId: req.user.id }
+        })
+      } else {
+        await Pity.update({
+          standardGoldPity: gotGold ? 0 : currentUser.Pity.standardGoldPity,
+          standardPurplePity: gotPurple ? 0 : currentUser.Pity.standardPurplePity
+        }, {
+          where: { UserId: req.user.id }
+        })
+      }
       
       const message = {
         title: 'You won a 3 star',
@@ -25,60 +82,15 @@ class GachaController {
         rate: goldRate
       };
 
-      let gotGold = false;
-      let gotPurple = false;
-
-      if (currentUser.Pity.charLimitedPurplePity >= 10) { //Guara at 10 pull
+      if (gotPurple) {
         message.title = 'You won a 4 star';
-        currentUser.Pity.charLimitedPurplePity = 0;
-        gotPurple = true;
-      } else if (result <= purpleRate) { 
-        message.title = 'You won a 4 star';
-        currentUser.Pity.charLimitedPurplePity = 0;
-        gotPurple = true;
       }
 
-      if (currentUser.Pity.charLimitedGoldPity >= 76) { //Add chance to 20%
-        goldRate = 20000;
-        message.rate = goldRate;
-      }
-
-      if (currentUser.Pity.charLimitedGoldPity >= 90) { //Guara 5 star at 90
+      if (gotGold) {
         message.title = 'You won a 5 star';
-        currentUser.Pity.charLimitedGoldPity = 0;
-        gotPurple = false;
-        gotGold = true;
-      } else if (result <= goldRate) {
-        message.title = 'You won a 5 star';
-        currentUser.Pity.charLimitedGoldPity = 0;
-        gotPurple = false;
-        gotGold = true;
-      } 
-
-      if (currentBanner.type === 'limitedChar') {
-        await Pity.update({
-          charLimitedGoldPity: gotGold ? 0 : currentUser.Pity.charLimitedGoldPity + 1,
-          charLimitedPurplePity: gotPurple ? 0 : currentUser.Pity.charLimitedPurplePity + 1
-        }, {
-          where: { UserId: req.user.id }
-        })
-      } else if (currentBanner.type === 'limitedWeapon') {
-        await Pity.update({
-          weaponLimitedGoldPity: gotGold ? 0 : currentUser.Pity.weaponLimitedGoldPity + 1,
-          weaponLimitedPurplePity: gotPurple ? 0 : currentUser.Pity.weaponLimitedPurplePity + 1
-        }, {
-          where: { UserId: req.user.id }
-        })
-      } else {
-        await Pity.update({
-          standardGoldPity: gotGold ? 0 : currentUser.Pity.standardGoldPity + 1,
-          standardPurplePity: gotPurple ? 0 : currentUser.Pity.standardPurplePity + 1
-        }, {
-          where: { UserId: req.user.id }
-        })
       }
 
-      res.status(200).json({ message, result })
+      res.status(200).json({ message, result });
     } catch (error) {
       next(error);
     }
